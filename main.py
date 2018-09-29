9e6f83f19a6f846ab36273306a032cf2380064eb
Romul <r.zhdanov@socialquantum.com>
2018-09-29 16:02:57 +0300

    sanic now used

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3.7
 # -*- coding: utf-8 -*-
 
+from sanic.response import json as json_response
+from sanic_session import Session
+from sanic_jinja2 import SanicJinja2
 
-def main():
-    print('Фуксня 3.7 v 0.1')
-    return 0
+from sanic import Sanic
+app = Sanic()
+Session(app)
+jinja = SanicJinja2(app)
+app.static('/static', './static')
+
+
+@app.route('/')
+@jinja.template('index.html')
+async def test(request):
+    return {'info': 'в разработке :)'}
 
 
 if __name__ == '__main__':
-    main()
+    app.run(host='0.0.0.0', port=8901)
